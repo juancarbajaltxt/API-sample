3e161a99669064e49ddf7e6d748b162c7e39b7c7
TXT-52 <txt-52@MacBook-Pro-5.lan1>
2020-06-29 22:55:02 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weatherapp';
+  title = 'openweather';
+
+  // variables that will hold server data 
+  // cities rated best global cities one color 
+  // cities rated destinations for 2026 world cup another color
+  // cities for both (only a couple) a different color
+  // legend
+  houston_data;
+  la_data;
+
+  // our HttpClient Object that is constructed here is called http 
+  constructor(public http: HttpClient){
+    // los angeles
+    this.http.get('https://api.openweathermap.org/data/2.5/weather?id=5368361&units=imperial&APPID=4e28d51992b0332264a6fdc6011d6d71')
+    .subscribe((la_data) => {
+      console.log(la_data);
+      this.la_data = la_data;
+    }, (err) => {console.log(err);
+    })
+
+    //houston
+    this.http.get('https://api.openweathermap.org/data/2.5/weather?id=4699066&units=imperial&APPID=4e28d51992b0332264a6fdc6011d6d71')
+    .subscribe((houston_data) => {
+      console.log(houston_data);
+      this.houston_data = houston_data;
+    }, (err) => {console.log(err);
+    })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,6 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+     // import HttpClientModule after BrowserModule.
+     HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
